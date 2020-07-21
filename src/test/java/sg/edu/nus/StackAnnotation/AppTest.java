ec4a1f946b35325080e7c4b3b85dc8c28050f049
Jerald <tohjinxiong@gmail.com>
2020-07-21 14:15:26 +0800

    fail the test case

--- src/test/java/sg/edu/nus/StackAnnotation/AppTest.java
+++ src/test/java/sg/edu/nus/StackAnnotation/AppTest.java
@@ -1,4 +1,4 @@
-package sg.edu.nus.StackAnnotation;
+]package sg.edu.nus.StackAnnotation;
 
 import static org.junit.Assert.assertTrue;
 
@@ -15,6 +15,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue( false );
     }
 }
